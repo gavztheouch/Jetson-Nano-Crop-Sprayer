b0596e69e83f7c06b1e74783eed8ea5d445231f0
Gavztheouch <32553777+gavztheouch@users.noreply.github.com>
2020-02-07 18:23:42 +0000

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -1,13 +1,10 @@
-#include <Encoder.h>
 int value=0;
-const byte sprayheads = 48;             // number of sprayheads
-byte Spray[48];                        //an array to store spray solenoid postions
+const byte sprayheads = 1;             // number of sprayheads
+byte Spray[1];                        //an array to store spray solenoid postions
 int dist = 0;
 boolean newData = false;
 boolean showNewDataB = false;
 int rowshift = 0;
-Encoder myencoder(2, 3);
-long newmyencoder;
 int temprowshift;
 
 void setup() 
@@ -37,30 +34,20 @@ void setup()
       
    }
  
-void loop() 
-   {
-      
-      newmyencoder = myencoder.read();
-      temprowshift = map(newmyencoder, 0, 3000, 0, 6); //map value of encoder or pot to 0-5
-      rowshift = temprowshift * 8;                     //multiply map value by 8 to get rowshift value
-      //Serial.print(rowshift);
-      if(newmyencoder >= 3000){
-        myencoder.write(0);
-        digitalWrite(11, LOW);
-        
-      }
+void loop() {
+   
       recvWithEndMarker();
       if (showNewDataB == true){
         showNewData();
       }
-      if (rowshift >= 47) {
+      if (rowshift >= 1) {
         rowshift = 0;
         digitalWrite(11, LOW);
         showNewDataB = false;
         resetspray();
         }
      
-      delay(1000);
+     // delay(1000);
 
    }
 
@@ -92,30 +79,20 @@ void recvWithEndMarker() {
 
 void showNewData() {
 
-  int head = 0;         
-  int heads = 6;
-  int pinshift = 4;     //makes sure the correct pins are used, we are starting from 8
-  int a = 0;
-  int bito;
-
-  while ( head < heads ){
-
-    
-    a = head + pinshift;
-    bito = head + rowshift;
   
-        if (Spray[bito] == '1')
-        digitalWrite (a, LOW);
+  
+        if (Spray[0] == '1')
+        digitalWrite (4, LOW);
        
      
-        else if (Spray[bito] == '0')
-        digitalWrite (a, HIGH);
-        
-        head++;
+        else if (Spray[0] == '0')
+        digitalWrite (4, HIGH);
         
         
-       }
-        if (rowshift>= 48);{
+        
+        
+       
+        if (rowshift>= 1);{
         newData = false;}
         
     }
